57000d4ceb70109c4685c208cafdf307f6243ad9
Malaika <malaikaaptech@gmail.com>
2023-11-26 01:56:55 -0800

    m

---
+++ java.js
@@ -0,0 +1,78 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+}, {
+    "id": 1,
+    "title": "Maya Modern Dining Table Set With 6 Chairs",
+    "price": 179999,
+    "description": "Introducing the Maya Modern Dining Table Set with 6 Chairs from Al-Mateen Furniture. This dining set is the perfect blend of modern elegance and functionality, featuring an MDF top dining table with a wood base.",
+    "brand": "Mateen ",
+    "image": "https://almateenhome.com/cdn/shop/products/maya-modern-dining-table-set-with-6-chairs-1.jpg?v=1688497110"
+}, {
+    "id": 2,
+    "title": "Bogota Wooden Dining Table Set With 6 Chairs",
+    "price": 159999,
+    "description": "This dining set is a perfect blend of beauty and functionality, featuring a wooden top dining table with a wood base and six comfortable chairs upholstered in washable textured velvet with wooden bases",
+    "brand": "Mateen",
+    "image": "https://almateenhome.com/cdn/shop/products/bogota-wooden-dining-table-set-with-6-chairs-1.jpg?v=1688497100"
+}, {
+    "id": 3,
+    "title": "Twin Wood Bed",
+    "price": 130000,
+    "description": "with an elevated sleep space that’s reached via ladders, we recommend that these high sleeper beds (loft beds) are also used by children above 6, making them only suitable for older children.",
+    "brand": "The Home Depo",
+    "image": "https://images.thdstatic.com/productImages/d112f41e-8898-4c0d-9a11-0dd305b3f241/svn/gray-bunk-beds-t-01776-e-64_600.jpg"
+}, {
+    "id": 4,
+    "title": "Canopy Bed",
+    "price": 120000,
+    "description": "Inspired by a blend of traditional & contemporary designs, the Edler Double Bed comes with storage drawers underneath and keeps it chic yet classic. It's durable, spacious and would last a lifetime as your perfect roomie. ",
+    "brand": "Freedom Furniture",
+    "image": "https://api-prod.freedom.com.au/medias/24517393-LIF-1-ORIGINAL-800Wx800H?context=bWFzdGVyfGltYWdlc3w2MDQ4MTh8aW1hZ2UvanBlZ3xoOGEvaDJlLzkyNDcyNTUxMzQyMzgvMjQ1MTczOTNfTElGXzFfT1JJR0lOQUxfODAwV3g4MDBIfGQ3MjEyYWQzYWU2ZjQzNDYwNTU5N2E2ZTA3MTg4NzJiOTUwZTg2MzAzYmRhZDVhYmRlN2RkZjQ2NjZiZjQ3MTM"
+}, {
+    "id": 5,
+    "title": "Luxury Nesting Side Table",
+    "price": 9399,
+    "description": "Use as an accent table or convenient work surface",
+    "brand": "My Home Style",
+    "image": "https://myhomestyle.pk/cdn/shop/products/094235b6-8318-4691-ba1a-b7949e24ac7b.jpg?v=1666335310"
+}, {
+    "id": 6,
+    "title": "Coffee Side Table",
+    "price": 7999,
+    "description": "Material: MDF Marble Sheet (Top), Metal(Base)",
+    "brand": "92bleeding",
+    "image": "https://92beddingstore.pk/cdn/shop/products/5b0f6e2e-c51b-43b5-85d0-2ad560e8ac50.jpg?v=1688721520"
+}, {
+    "id": 7,
+    "title": "Sofa",
+    "price": 107999,
+    "description": "It’s a piece of furniture that usually has two arms that rest on the ground and can be used as a place to sit or lay down.",
+    "brand": "Housing",
+    "image": "https://housing.com/news/wp-content/uploads/2022/11/stylish-modern-sofa-design-compressed.jpg"
+}, {
+    "id": 8,
+    "title": "Elegant Sofa",
+    "price": 32999,
+    "description": " It can be used for a variety of purposes like watching TV, reading a book, or spending some time with friends. ",
+    "brand": "Lujain",
+    "image": "https://cdn.shopify.com/s/files/1/0022/0004/3556/products/Brittany_Convertible_Sofa_300x300.jpg?v=1527350017"
+}, {
+    "id": 9,
+    "title": "Office Furni",
+    "price": 20499,
+    "description": "Our modern home office furniture is thoughtfully designed and expertly crafted to create comfort while being productive.",
+    "brand": "Jensin-Lewis",
+    "image": "https://www.jensen-lewis.com/cdn/shop/collections/sigma-office-furniture-BDI-modern-desk-storage-3.jpg?v=1576175115"
+}, {
+    "id": 10,
+    "title": "Office Furniture",
+    "price": 16299,
+    "description": "Modern Home Office Furniture, Contemporary New York",
+    "brand": "vlr",
+    "image": "https://copenhagen-imports.com/wp-content/uploads/2019/09/bdi-sigma-office-desk-06.jpg"
+}]
